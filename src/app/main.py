15715534370ba933a40157547fde4b93bdf45a6e
Etienne Fischer <etienne.fischer@totalenergies.com>
2025-07-02 10:22:02 +0200

    feat: open connection to frontend

--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from src.app.api.routes import router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 app.include_router(router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:7567"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
